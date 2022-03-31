419c7d29679a8a6df847bbff06bb042fb3a18639
HuuRR <felipe.campregher@gmail.com>
2022-03-31 13:48:24 -0300

    teste

---
+++ script.js
@@ -0,0 +1,12 @@
+const main = document.getElementsByClassName("main")
+const container = document.createElement("section")
+container.classList.add("tabuleiro")
+container.innerHTML = `
+    <div class="botao botao-verde"></div>
+    <div class="botao botao-amarelo"></div>
+    <div class="botao botao-vermelho"></div>
+    <div class="botao botao-azul"></div>
+    <div class="comando"></div>
+`
+
+
